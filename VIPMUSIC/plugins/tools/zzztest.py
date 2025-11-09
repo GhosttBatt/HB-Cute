86b592e967c02ce9e4b710350e3ffe4869f5934a
GhosttBatt <ghosttbatt@gmail.com>
2025-11-09 22:05:04 +0530

    Update zzztest.py

--- VIPMUSIC/plugins/tools/zzztest.py
+++ VIPMUSIC/plugins/tools/zzztest.py
@@ -1,6 +1,17 @@
 from VIPMUSIC import app
 from pyrogram import filters
+import config
 
-@app.on_message(filters.command("zzztest") & filters.group)
+SUDOERS = list(map(int, config.SUDOERS))
+
+group_filter = filters.chat_type.groups
+
+def is_authorized(_, __, message):
+    user = message.from_user
+    if not user:
+        return False
+    return user.id in SUDOERS or user.id == config.OWNER_ID or message.from_user.is_chat_admin
+
+@app.on_message(filters.command("zzztest") & group_filter & filters.create(is_authorized))
 async def zzz_test(_, message):
     await message.reply_text("✅ ZZZ Test works!")
