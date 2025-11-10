d3909a0322edc30376d186fe40d291ca7e6c9061
GhosttBatt <ghosttbatt@gmail.com>
2025-11-10 21:44:14 +0530

    crush.py

--- VIPMUSIC/plugins/bot/crush.py
+++ VIPMUSIC/plugins/bot/crush.py
@@ -11,6 +11,8 @@ from time import time
 import asyncio
 from VIPMUSIC.utils.extraction import extract_user
 
+print("[crush] crush")
+
 # Define a dictionary to track the last message timestamp for each user
 user_last_message_time = {}
 user_command_count = {}
