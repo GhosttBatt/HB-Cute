2090cc8d5b636deb50021137bf2580bb3ac7377c
GhosttBatt <ghosttbatt@gmail.com>
2025-11-10 22:12:16 +0530

    playlist.py

--- VIPMUSIC/plugins/play/playlist.py
+++ VIPMUSIC/plugins/play/playlist.py
@@ -35,6 +35,8 @@ from time import time
 import asyncio
 from VIPMUSIC.utils.extraction import extract_user
 
+print("[playlist] addplaylist, delplaylist, playplaylist")
+
 # Define a dictionary to track the last message timestamp for each user
 user_last_message_time = {}
 user_command_count = {}
